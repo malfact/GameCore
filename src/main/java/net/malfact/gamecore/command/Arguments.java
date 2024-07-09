dd1d4368e34fc4770a47242b84c5662766712096
malfact <themalfact@gmail.com>
2024-07-09 15:18:17 -0500

    Added argument for Game

--- src/main/java/net/malfact/gamecore/command/Arguments.java
+++ src/main/java/net/malfact/gamecore/command/Arguments.java
@@ -1,8 +1,12 @@
 package net.malfact.gamecore.command;
 
-import dev.jorel.commandapi.arguments.*;
+import dev.jorel.commandapi.arguments.Argument;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.CustomArgument;
+import dev.jorel.commandapi.arguments.StringArgument;
 import net.malfact.gamecore.GameCore;
 import net.malfact.gamecore.Messages;
+import net.malfact.gamecore.game.Game;
 import net.malfact.gamecore.queue.GameQueue;
 import net.malfact.gamecore.team.GameTeam;
 
@@ -29,5 +33,17 @@ public class Arguments {
                 return team;
 
         }).replaceSuggestions(ArgumentSuggestions.strings(info -> GameCore.getTeamManager().getTeamNames()));
+    }
+
+    public static Argument<Game> Game(String nodeName) {
+        return new CustomArgument<>(new StringArgument(nodeName), info -> {
+            Game game = GameCore.gameManager().getGame(info.input());
+
+            if (game == null)
+                throw CustomArgument.CustomArgumentException.fromAdventureComponent(Messages.get("GAME_UNKNOWN", info.input()));
+            else
+                return game;
+
+        }).replaceSuggestions(ArgumentSuggestions.strings(info -> GameCore.gameManager().getGameNames()));
     }
 }
