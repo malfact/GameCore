ceaade8e3e6d662852792709425c067b997aa18a
malfact <themalfact@gmail.com>
2024-07-09 19:53:25 -0500

    More Functions for GameLib

--- src/main/java/net/malfact/gamecore/lua/GameLib.java
+++ src/main/java/net/malfact/gamecore/lua/GameLib.java
@@ -1,5 +1,6 @@
 package net.malfact.gamecore.lua;
 
+import net.malfact.gamecore.GameCore;
 import net.malfact.gamecore.api.LuaApi;
 import net.malfact.gamecore.api.LuaUtil;
 import net.malfact.gamecore.game.Game;
@@ -13,8 +14,12 @@ import org.luaj.vm2.LuaValue;
 
 public class GameLib extends EventLib {
 
-    private final LuaFunction func_getPlayers = LuaUtil.toFunction(this::getPlayers);
-    private final LuaFunction func_getEntities = LuaUtil.toFunction(this::getEntities);
+    private final LuaFunction func_getPlayers =     LuaUtil.toFunction(this::getPlayers);
+    private final LuaFunction func_getEntities =    LuaUtil.toFunction(this::getEntities);
+    private final LuaFunction func_joinGame =       LuaUtil.toFunction(this::joinGame);
+    private final LuaFunction func_leaveGame =      LuaUtil.toFunction(this::leaveGame);
+    private final LuaFunction func_isPlaying =      LuaUtil.toFunction(this::isPlaying);
+    private final LuaFunction func_stop =           LuaUtil.toFunction(this::stopGame);
 
     public GameLib(Game instance) {
         super(instance, "game");
@@ -24,8 +29,12 @@ public class GameLib extends EventLib {
     protected LuaValue createLib(LuaValue env) {
         LuaTable lib = new LuaTable();
 
-        lib.set("getPlayers",   func_getPlayers);
+        lib.set("getPlayers ",  func_getPlayers);
         lib.set("getEntities",  func_getEntities);
+        lib.set("joinGame",     func_joinGame);
+        lib.set("leaveGame",    func_leaveGame);
+        lib.set("isPlaying",    func_isPlaying);
+        lib.set("stop",         func_stop);
 
         env.set("Game", lib);
 
@@ -78,5 +87,41 @@ public class GameLib extends EventLib {
         }
 
         return table;
+    }
+
+    private LuaValue joinGame(LuaValue arg) {
+        Player player = arg.checkuserdata(Player.class);
+        Game game = GameCore.gameManager().getGame(player);
+
+        if (game != null)
+            return LuaConstant.FALSE;
+
+        return LuaValue.valueOf(GameCore.gameManager().joinGame(player, instance));
+    }
+
+    private LuaValue leaveGame(LuaValue arg) {
+        Player player = arg.checkuserdata(Player.class);
+        Game game = GameCore.gameManager().getGame(player);
+        if (game == null)
+            return LuaConstant.FALSE;
+
+        if (!game.getName().equals(instance.getName()))
+            return LuaConstant.FALSE;
+
+        return LuaValue.valueOf(GameCore.gameManager().leaveGame(player));
+    }
+
+    private LuaValue isPlaying(LuaValue arg) {
+        Player player = arg.checkuserdata(Player.class);
+        Game game = GameCore.gameManager().getGame(player);
+
+        if (game == null)
+            return LuaConstant.FALSE;
+
+        return LuaApi.valueOf(game.getName().equals(instance.getName()));
+    }
+
+    private void stopGame() {
+        instance.stop();
     }
 }
